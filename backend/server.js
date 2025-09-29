db8188841a95e52a0e41adcf194ab56f92f92b04
Piyush Varshney <nectarbarahseni21724@gmail.com>
2025-09-29 20:59:11 +0530

    Fix __dirname duplicate declaration error in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001"], // Frontend URLs
     credentials: true
 }));
-const __dirname = path.resolve();
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/Frontend/frontend/build')));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'Frontend', 'frontend', 'build', 'index.html')));
